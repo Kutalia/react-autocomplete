36c56e1bbfdabdaa374e9d192c6341b030ae222a
Kote Kutalia <kotekutalia@gmail.com>
2022-06-02 21:09:27 +0400

    autocomplete search algorithm added

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,47 @@
 import React from 'react';
 
 import Banner from './components/Banner';
+import AutoComplete, { DataItem } from './components/AutoComplete';
 import './App.css';
+import users from './users.json';
 
 function App() {
+  const getData = (query: string): Promise<DataItem[]> => {
+    return new Promise((resolve) => {
+      const filteredUsersByName = users.users.filter(({ firstName, lastName, maidenName }) => {
+        const chunks = query.split(' ').map((chunk) => chunk.toLowerCase());
+
+        return chunks.some((chunk) => {
+          return firstName.toLowerCase().indexOf(chunk) > -1
+            || lastName.toLowerCase().indexOf(chunk) > -1
+            || maidenName.toLowerCase().indexOf(chunk) > -1
+        });
+      }).map(({ firstName, lastName, id }) => ([
+        firstName + ' ' + lastName,
+        id,
+      ]));
+
+      // Getting unique user names
+      const finalData: DataItem[] = [];
+      const dataMap = new Map(filteredUsersByName as [number, string][]);
+      
+      dataMap.forEach((id, name) => {
+        finalData.push({
+          data: name,
+          id,
+        });
+      });
+
+      setTimeout(() => {
+        resolve(finalData.slice(0, 15));
+      }, 200);
+    });
+  }
+
   return (
     <div className="App">
       <Banner title="Header" />
+      <AutoComplete getData={getData} />
       <Banner title="Header" placement="bottom" />
     </div>
   );
--- src/components/AutoComplete/index.tsx
+++ src/components/AutoComplete/index.tsx
@@ -1,7 +1,83 @@
-const AutoComplete = () => {
-  return (<div>
+import React, { useState, useEffect, useLayoutEffect } from 'react';
 
-  </div>);
+import useWindowResize, { ScreenSizes } from '../../hooks/useWindowResize';
+import './AutoComplete.css';
+
+export interface DataItem {
+  data: string | number;
+  id?: React.Key;
+}
+
+interface PropTypes {
+  getData: (query: string) => Promise<DataItem[]>;
+}
+
+const AutoComplete: React.FC<PropTypes> = ({ getData }) => {
+  const [items, setItems] = useState<DataItem[]>([]);
+  const [query, setQuery] = useState<string>('');
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+  const [inputEl, setInputEl] = useState<HTMLInputElement | null>(null);
+  const [autoCompleteEl, setAutoCompleteEl] = useState<HTMLDivElement | null>(null);
+  const windowSize = useWindowResize();
+
+  useEffect(() => {
+    const searchStr = query?.trim();
+
+    if (searchStr) {
+      getData(searchStr).then((data) => {
+        setItems(data);
+      });
+    }
+  }, [query]);
+
+  useLayoutEffect(() => {
+    if (inputEl && autoCompleteEl) {
+      autoCompleteEl.style.top = `${inputEl.clientHeight}px`;
+    }
+  }, [inputEl, autoCompleteEl]);
+
+  useEffect(() => {
+    function handler(e: UIEvent) {
+      if (e.target !== inputEl) {
+        setIsOpen(false);
+      }
+    }
+
+    if (inputEl) {
+      window.addEventListener('click', handler);
+
+      return () => window.removeEventListener('click', handler);
+    }
+  }, [inputEl]);
+
+  return (
+    <div className="autocomplete">
+      <input
+        ref={setInputEl}
+        value={query}
+        placeholder="Enter user name"
+        onClick={() => query && setIsOpen(true)}
+        onChange={(e) => {
+          setQuery(e.target.value);
+          setIsOpen(true);
+        }}
+      />
+      {isOpen && !!items.length &&
+        <div className="autocomplete__menu" ref={setAutoCompleteEl}>
+          {items.map(({ data, id }, index) => (
+            <div
+              key={id ?? index}
+              onClick={() => {
+                setQuery(String(data));
+                setIsOpen(false);
+              }}
+            >
+              {data}
+            </div>
+          ))}
+        </div>}
+    </div>
+  );
 };
 
 export default AutoComplete;
